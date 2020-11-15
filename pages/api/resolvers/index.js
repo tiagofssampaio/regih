2193a4f4c87bc24e66048ad4a40cbc7ba4a4ecfd
Tiago Sampaio <tiagofssampaio@gmail.com>
2020-11-15 21:34:53 +0000

    TODO

--- pages/api/resolvers/index.js
+++ pages/api/resolvers/index.js
@@ -3,23 +3,44 @@ import escape from 'sql-template-strings';
 
 export const resolvers = {
     Query: {
+        client: async (_, args) => {
+            /**
+             * TODO
+             * get client
+             */
+        },
         clients: async () => {
+            /**
+             * TODO
+             * pagination and filters
+             * relation with task and task_time
+             */
             return await db.query(escape`
               SELECT *
               FROM client
               ORDER BY client_id
             `);
         },
-        client: async (_, args) => {
-        },
 
         task: async (_, args) => {
+            /**
+             * TODO
+             * get task with all relations
+             */
         },
         tasks: async (_, args) => {
+            /**
+             * TODO
+             * pagination and filters
+             * relation with client, task_time, invoice
+             */
         }
     },
     Mutation: {
         createClient: async (_, args) => {
+            return await db.query(escape`
+                INSERT INTO client (name, vat_id, rate) VALUES ('${args.name}', '${args.vat_id}', ${args.rate})
+            `)
         },
         updateClient: async (_, args) => {
         },
